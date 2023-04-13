6600a6878f6e7ede6a30435e645e986bc29941b7
MariannElizabeth <mariana.elizabeth13mr@gmail.com>
2023-04-13 17:43:06 -0600

    Inputs & Outputs

---
+++ 01-Entradas-Salidas/Tipo-de-Datos/ejercicio.js
@@ -0,0 +1,51 @@
+//Declara una variable
+var nombre;
+console.log("line 2 ~ Variable nombre", nombre)
+
+//Declarar y asignar una variable
+var mascotas= "perro";
+
+console.log("line 6 ~ Variable mascotas asignada a:", mascotas)
+
+//Asignar el valor de la primer variable
+nombre= 'Marian';
+console.log("line 11 ~ Variable nombre asignada a", nombre)
+
+//Declara una variable por cada tipo de dato y asignale un valor, solo datos primitivos
+//DATOS PRIMITIVOS
+// 1. null
+var SinValor= null;
+// 2. undefined
+var Indefinido= undefined;
+// 3. boleano
+var boleano=true;
+// var falso= false;
+// 4. numero
+var number= '13'
+// 5. String
+var string= 'Cadena de caracteres'
+// 6. Símbolo 
+var symbol= Symbol('Simbolo')
+
+// Imprime en consola un suma
+console.log('La suma de dos números =', 1+1);
+
+// Declara y asigna dos variables numéricas e imprime en consola la suma de esas variables
+var a= 8;
+var b= 13;
+console.log("La suma de a + b =", a + b)
+
+// Operadores matemáticos
+//Suma +
+console.log(4+4);
+//Resta -
+console.log(4-4);
+//Multiplicación *
+console.log(4*4);
+// División /
+console.log(4/4);
+// Residuo de la división %
+console.log(14%4);
+// Exponencial
+console.log(4**4);
+
---
+++ 01-Entradas-Salidas/salidas/index.js
@@ -0,0 +1,5 @@
+alert("Eres el GANADOR del sorteo de 1,000,000,000 de pesos")
+
+document.write("SORTEO MAYOR DE LA LOTERIA NACIONAL")
+
+console.log("NUESTRO AFORTUNADO GANADOR ES", prompt("Escribe tu nombre para reclamar tu premio")) 
